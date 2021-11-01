b88426ed8caef8b386b82734eefe3ad197b553ab
Ronel R. Gumahin Jr <ron.gumahin21@gmail.com>
2021-11-01 23:13:31 +0800

    my-update

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Main template file.
+ * 
+ * @package Aquila
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+<body>
+    Hello WordPress
+</body>
+</html>
