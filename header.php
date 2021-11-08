7dd57bdf05fd3013b40d3ee63e27ceea01853b43
Ronel R. Gumahin Jr <ron.gumahin21@gmail.com>
2021-11-08 11:29:30 +0800

    changes on nav and header

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-quiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>> 
